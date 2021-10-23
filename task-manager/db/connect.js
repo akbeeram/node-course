891fbe536a79e24e920d575b9f02bd19a33135da
Anell Kumarr Beeram <anellkumarrbeeram@gmail.com>
2021-10-23 19:02:19 +0530

    remove mongo uri

--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-// const connectionString = 'mongodb+srv://anell:Apache@cluster0.boi4h.mongodb.net/task-manager?retryWrites=true&w=majority';
-
 const connectDB = (url) => {
 return mongoose.connect(url);
 }
